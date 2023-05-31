3fc99f81a6ed0abee62795162943a4bdf1a12513
Stefano Colla <ste.colla@gmail.com>
2023-05-31 13:12:43 +0200

    First Commit

---
+++ cameras/src/main/java/it/serravalle/cameras/config/MediaServer.java
@@ -0,0 +1,43 @@
+package it.serravalle.cameras.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "app.mediaserver")
+public class MediaServer {
+
+    /**
+     * Hostname of MediaServer.
+     */
+    private String hostname;
+    private String user;
+    private String password;
+    
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getHostname() {
+		return hostname;
+	}
+
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+
+    
+}
